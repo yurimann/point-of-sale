f27ec4ee65a01f7a7098b5231207fda7809a32b3
yurimann <yuri.manahan@gmail.com>
2018-01-01 20:37:44 -0500

    Initial setup

---
+++ src/Keypad.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Keypad extends Component {
+  render() {
+    return (
+      <div className="Keypad">
+        <h1 className="Keypad-title">Keypad values</h1>
+      </div>
+    );
+  }
+}
+
+export default Keypad;
